aca143caaaad502a9b3eae672e2ed5a46a56014c
jhonatan-chaves <cjonata799@gmail.com>
2023-09-28 09:48:50 -0300

    iniciando um projeto web em C-Sharp, para controle de contatos

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContato.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
